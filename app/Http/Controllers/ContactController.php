4069e4dc2a725f5b751003701aade61be2806aef
Bikram Upadhyay <upadhyaybikram@gmail.com>
2024-01-16 19:19:46 +1100

    #store function updated for storing guest details

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -4,44 +4,44 @@ namespace App\Http\Controllers;
 
 use App\Models\Guest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 use Illuminate\Validation\Rule;
+use Illuminate\Validation\ValidationException;
 
 class ContactController extends Controller
 {
     public function index()
     {
-       // return view('comment');
         return view('welcome');
     }
 
-
     public function store(Request $request)
     {
-      //  dd($request);
-        $validatedData = $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => [
-                'required',
-                'email',
-                'max:255',
-                Rule::unique('guests', 'email'), // Check uniqueness in the "guests" table
-            ],
-            'subject' => 'required|string|max:255',
-            'message' => 'required|string',
-        ]);
-     
-
+      //  dd($request->input());
         try {
+            $validatedData = $request->validate([
+                'name' => 'required|string|max:255',
+                'email' => [
+                    'required',
+                    'email',
+                    'max:255',
+                    Rule::unique('guests', 'email')
+                ],
+                'subject' => 'required|string|max:255',
+                'message' => 'required|string',
+            ]);
+
             $guest = new Guest($validatedData);
             $guest->save();
 
-        } catch(\Exception $e) {
-            dd($e);
-            return response()->json(['error' => 'Email is not unique.'], 422);
+            return redirect('/#contact')->with('success', 'Thank you! Your message has been submitted successfully.
+            One of our agent will contact you soon. ');
+
+        } catch (ValidationException $e) {
+            $errors = $e->validator->errors()->all();
+           // return response()->json(['error' => $errors], 422);
+            return redirect('/#contact')->with('error', 'Oops! This user has already been registered');
 
         }
-        
-        return redirect('/');
-
     }
 }
