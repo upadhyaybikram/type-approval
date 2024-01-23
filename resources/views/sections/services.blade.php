5486c8aa6ebd3be64f8f8f27fabff33fa43869f0
Bikram Upadhyay <upadhyaybikram@gmail.com>
2024-01-23 11:59:32 +1100

    #service section updated

--- resources/views/sections/services.blade.php
+++ resources/views/sections/services.blade.php
@@ -4,7 +4,7 @@
       <!--  Section Title -->
       <div class="container section-title" data-aos="fade-up">
         <h2>Services</h2>
-        <p>Necessitatibus eius consequatur ex aliquid fuga eum quidem sint consectetur velit</p>
+        <p>We have a dynamic team always ready to help the customers be it for type approval certificate or responsive web development</p>
       </div><!-- End Section Title -->
 
       <div class="container">
@@ -15,8 +15,20 @@
             <div class="service-item d-flex">
               <div class="icon flex-shrink-0"><i class="bi bi-briefcase"></i></div>
               <div>
-                <h4 class="title"><a href="services-details.html" class="stretched-link">Lorem Ipsum</a></h4>
-                <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
+                <h4 class="title">Type Approval Certificate Services</h4>
+                  <p>
+                      <i>Local Representation</i>
+                  </p>
+                  <p class="description">We serve as your trusted local representative in
+                      Nepal and beyond, facilitating radio device manufacturers in obtaining
+                      swift and efficient type approval certificates.
+                  </p>
+
+                  <p>
+                      <i>Timely Certifications</i>
+                  </p>
+                  <p class="description">Benefit from our expertise as one of Nepal's leading agents,
+                      ensuring your certificates are secured within a timeframe of 2-4 weeks.</p>
               </div>
             </div>
           </div>
@@ -26,8 +38,11 @@
             <div class="service-item d-flex">
               <div class="icon flex-shrink-0"><i class="bi bi-card-checklist"></i></div>
               <div>
-                <h4 class="title"><a href="services-details.html" class="stretched-link">Dolor Sitema</a></h4>
-                <p class="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata</p>
+                  <h4 class="title">Letter of Exemption</h4>
+                <p class="description">Navigate regulatory requirements seamlessly with our assistance
+                    in obtaining Letters of Exemption for Low Power Devices (LPD) and Short Range Devices (SRD).
+                </p>
+                  <p class="description">This option is applicable only for equipment with coverage strictly within 15 meters.</p>
               </div>
             </div>
           </div><!-- End Service Item -->
@@ -36,41 +51,35 @@
             <div class="service-item d-flex">
               <div class="icon flex-shrink-0"><i class="bi bi-bar-chart"></i></div>
               <div>
-                <h4 class="title"><a href="services-details.html" class="stretched-link">Sed ut perspiciatis</a></h4>
-                <p class="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
+                <h4 class="title">Web Design and Development Solutions</h4>
+                  <p><i>Dynamic Development Team</i></p>
+                  <p class="description">Type Approval and IT Solutions boasts a dynamic team of
+                      experienced developers dedicated to providing cutting-edge solutions.
+                  </p>
+
+                  <p><i>Unique Designs</i></p>
+                  <p class="description">Whether you're seeking a personal or company portfolio or a product design with unique solutions,
+                      we're ready to bring your vision to life.
+                  </p>
               </div>
             </div>
           </div><!-- End Service Item -->
 
-          <div class="col-lg-6 " data-aos="fade-up" data-aos-delay="400">
-            <div class="service-item d-flex">
-              <div class="icon flex-shrink-0"><i class="bi bi-binoculars"></i></div>
-              <div>
-                <h4 class="title"><a href="services-details.html" class="stretched-link">Magni Dolores</a></h4>
-                <p class="description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-              </div>
-            </div>
-          </div><!-- End Service Item -->
+            <div class="col-lg-6 " data-aos="fade-up" data-aos-delay="300">
+                <div class="service-item d-flex">
+                    <div class="icon flex-shrink-0"><i class="bi bi-bar-chart"></i></div>
+                    <div>
+                        <h4 class="title">Freelancer Projects</h4>
+                        <p><i>Dynamic Development Team</i></p>
+                        <p class="description">Ensure resource mobilization and continuous development with our assistance
+                            on freelancer projects.
+                        </p>
 
-          <div class="col-lg-6 " data-aos="fade-up" data-aos-delay="500">
-            <div class="service-item d-flex">
-              <div class="icon flex-shrink-0"><i class="bi bi-brightness-high"></i></div>
-              <div>
-                <h4 class="title"><a href="services-details.html" class="stretched-link">Nemo Enim</a></h4>
-                <p class="description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque</p>
-              </div>
-            </div>
-          </div><!-- End Service Item -->
-
-          <div class="col-lg-6 " data-aos="fade-up" data-aos-delay="600">
-            <div class="service-item d-flex">
-              <div class="icon flex-shrink-0"><i class="bi bi-calendar4-week"></i></div>
-              <div>
-                <h4 class="title"><a href="services-details.html" class="stretched-link">Eiusmod Tempor</a></h4>
-                <p class="description">Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi</p>
-              </div>
-            </div>
-          </div><!-- End Service Item -->
+                        <p class="description">Our team is equipped to contribute to your project's success.
+                        </p>
+                    </div>
+                </div>
+            </div><!-- End Service Item -->
 
         </div>
 
