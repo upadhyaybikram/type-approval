4ddb0d481bc1734ffc9b5752cff253b631a85cae
Bikram Upadhyay <upadhyaybikram@gmail.com>
2024-01-25 21:56:55 +1100

    #unused file removed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -125,10 +125,6 @@
     <div class="container copyright text-center mt-4">
       <p>&copy; <span>Copyright</span> <strong class="px-1">Type Approval and IT Solutions</strong> <span>All Rights Reserved</span></p>
       <div class="credits">
-        <!-- All the links in the footer should remain intact. -->
-        <!-- You can delete the links only if you've purchased the pro version. -->
-        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-        <!-- Purchase the pro version with working PHP/AJAX contact form: [buy-url] -->
         Designed and developed by <a href="https://www.linkedin.com/in/bikram-upadhyay-0a670b94/" target="_blank">Bikram Upadhyay</a>
       </div>
     </div>
