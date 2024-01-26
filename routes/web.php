ed283636e528352b85e761c565c19ccf0f2906f7
Bikram Upadhyay <upadhyaybikram@gmail.com>
2024-01-26 18:00:32 +1100

    #test

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,6 @@ use Illuminate\Support\Facades\URL;
 |
 */
 
-if($this->app->environment('production')) {
-    URL::forceScheme('https');
-}
 
 Route::get('/', function () {
     return view('welcome');
