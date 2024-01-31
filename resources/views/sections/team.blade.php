0a2f5e0a7ca0ca0479906fa6349e495237d32c2a
Bikram Upadhyay <upadhyaybikram@gmail.com>
2024-02-01 02:12:46 +1100

    #minor updates

--- resources/views/sections/team.blade.php
+++ resources/views/sections/team.blade.php
@@ -11,26 +11,7 @@
 
         <div class="row gy-5">
 
-          <div class="col-lg-4 col-md-6 member" data-aos="fade-up" data-aos-delay="100">
-            <div class="member-img">
-              <img src="assets/img/team/team-head.jpg" class="img-fluid" alt="">
-              <div class="social">
-                <a href="#"><i class="bi bi-twitter"></i></a>
-                <a href="#"><i class="bi bi-facebook"></i></a>
-                <a href="#"><i class="bi bi-instagram"></i></a>
-                <a href="#"><i class="bi bi-linkedin"></i></a>
-              </div>
-            </div>
-            <div class="member-info text-center">
-              <h4>Bikram Upadhyay</h4>
-              <span>Principal Developer</span>
-              <p>Here comes our main developer, credit goes to him for this beautiful website. Along with the full stack web development,
-                  he is the one getting engaged with clients and share our mission.</p>
-
-            </div>
-          </div><!-- End Team Member -->
-
-          <div class="col-lg-4 col-md-6 member" data-aos="fade-up" data-aos-delay="200">
+          <div class="col-lg-6 col-md-6 member" data-aos="fade-up" data-aos-delay="200">
             <div class="member-img">
               <img src="assets/img/team/dularaj.jpg" class="img-fluid" alt="">
               <div class="social">
@@ -47,7 +28,7 @@
             </div>
           </div><!-- End Team Member -->
 
-          <div class="col-lg-4 col-md-6 member" data-aos="fade-up" data-aos-delay="300">
+          <div class="col-lg-6 col-md-6 member" data-aos="fade-up" data-aos-delay="300">
             <div class="member-img">
               <img src="assets/img/team/anil.jpg" class="img-fluid" alt="">
               <div class="social">
@@ -63,57 +44,6 @@
               <p>He is the piller of our company and maintains all the documentation and direct contact with the authority for quick acceptance of type approval application.</p>
             </div>
           </div><!-- End Team Member -->
-
-{{--          <div class="col-lg-4 col-md-6 member" data-aos="fade-up" data-aos-delay="400">--}}
-{{--            <div class="member-img">--}}
-{{--              <img src="assets/img/team/team-4.jpg" class="img-fluid" alt="">--}}
-{{--              <div class="social">--}}
-{{--                <a href="#"><i class="bi bi-twitter"></i></a>--}}
-{{--                <a href="#"><i class="bi bi-facebook"></i></a>--}}
-{{--                <a href="#"><i class="bi bi-instagram"></i></a>--}}
-{{--                <a href="#"><i class="bi bi-linkedin"></i></a>--}}
-{{--              </div>--}}
-{{--            </div>--}}
-{{--            <div class="member-info text-center">--}}
-{{--              <h4>Amanda Jepson</h4>--}}
-{{--              <span>Accountant</span>--}}
-{{--              <p>Magni voluptatem accusamus assumenda cum nisi aut qui dolorem voluptate sed et veniam quasi quam consectetur</p>--}}
-{{--            </div>--}}
-{{--          </div><!-- End Team Member -->--}}
-
-{{--          <div class="col-lg-4 col-md-6 member" data-aos="fade-up" data-aos-delay="500">--}}
-{{--            <div class="member-img">--}}
-{{--              <img src="assets/img/team/team-5.jpg" class="img-fluid" alt="">--}}
-{{--              <div class="social">--}}
-{{--                <a href="#"><i class="bi bi-twitter"></i></a>--}}
-{{--                <a href="#"><i class="bi bi-facebook"></i></a>--}}
-{{--                <a href="#"><i class="bi bi-instagram"></i></a>--}}
-{{--                <a href="#"><i class="bi bi-linkedin"></i></a>--}}
-{{--              </div>--}}
-{{--            </div>--}}
-{{--            <div class="member-info text-center">--}}
-{{--              <h4>Brian Doe</h4>--}}
-{{--              <span>Marketing</span>--}}
-{{--              <p>Qui consequuntur quos accusamus magnam quo est molestiae eius laboriosam sunt doloribus quia impedit laborum velit</p>--}}
-{{--            </div>--}}
-{{--          </div><!-- End Team Member -->--}}
-
-{{--          <div class="col-lg-4 col-md-6 member" data-aos="fade-up" data-aos-delay="600">--}}
-{{--            <div class="member-img">--}}
-{{--              <img src="assets/img/team/team1.jpg" class="img-fluid" alt="">--}}
-{{--              <div class="social">--}}
-{{--                <a href="#"><i class="bi bi-twitter"></i></a>--}}
-{{--                <a href="#"><i class="bi bi-facebook"></i></a>--}}
-{{--                <a href="#"><i class="bi bi-instagram"></i></a>--}}
-{{--                <a href="#"><i class="bi bi-linkedin"></i></a>--}}
-{{--              </div>--}}
-{{--            </div>--}}
-{{--            <div class="member-info text-center">--}}
-{{--              <h4>Josepha Palas</h4>--}}
-{{--              <span>Operation</span>--}}
-{{--              <p>Sint sint eveniet explicabo amet consequatur nesciunt error enim rerum earum et omnis fugit eligendi cupiditate vel</p>--}}
-{{--            </div>--}}
-{{--          </div><!-- End Team Member -->--}}
 
         </div>
 
